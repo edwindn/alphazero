9dfb0d1cc6ef966647c4d565958d98fc88fbe633
edwindn <edn.work@proton.me>
2024-12-18 18:36:47 +0000

    Update DCGAN.py

--- DCGAN.py
+++ DCGAN.py
@@ -183,6 +183,7 @@ class GAN(nn.Module):
 def train(epochs, dataset, batch_size=BATCH_SIZE):
     gan = GAN().to(device)
     dataloader = DataLoader(custom_dataset, batch_size, shuffle=True)
+    print(len(dataloader))
     for epoch in range(epochs):
         print(f'Epoch {epoch}')
         for batch in tqdm(iter(dataloader)):
