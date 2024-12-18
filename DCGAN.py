e97562c34fa120d73f210aa87610e6eef5da5ea8
edwindn <edn.work@proton.me>
2024-12-18 18:37:21 +0000

    Update DCGAN.py

--- DCGAN.py
+++ DCGAN.py
@@ -13,8 +13,8 @@ import psutil  # Import psutil for memory usage tracking
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-BATCH_SIZE = 64
-DATASET_LEN = 500
+BATCH_SIZE = 1
+DATASET_LEN = 10
 
 transform = Compose([
     Resize((224, 224)),
