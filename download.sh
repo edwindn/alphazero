7bfb7fb55d374e6f14077daaa6c63ccd7c35e5ad
edwindn <edn.work@proton.me>
2024-12-19 09:13:03 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,6 +7,7 @@ sudo apt install git
 sudo apt update
 sudo apt install unzip
 sudo apt update
+sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install torch torchvision numpy matplotlib tqdm psutil pillow
