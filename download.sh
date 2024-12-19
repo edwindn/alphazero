a92a466ed26ec89f61bb8e01bffc5a740a97105a
edwindn <edn.work@proton.me>
2024-12-19 09:14:54 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -10,7 +10,7 @@ sudo apt update
 sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
-pip install torch torchvision numpy matplotlib tqdm psutil pillow
+pip install torch torchvision numpy matplotlib tqdm psutil pillow gdown
 # download training data:
 gdown https://drive.google.com/uc?id=186yGq03kv1omNMyB9srZk6XJ8e35dLI8 -O img_align_celeba.zip
 unzip img_align_celeba.zip
