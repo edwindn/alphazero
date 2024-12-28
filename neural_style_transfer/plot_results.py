8c1eb3c7114cf5053c52c5c55e969fe5feb9ce9a
edwindn <edn.work@proton.me>
2024-12-28 15:03:15 +0000

    Update plot_results.py

--- neural_style_transfer/plot_results.py
+++ neural_style_transfer/plot_results.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-files = [f'generated_input_{i}.pt' for i in range(10)]
+files = [f'results/generated_input_{i}.pt' for i in range(10)]
 for i, f in enumerate(files):
     img = torch.load(f)
     img = img.squeeze(0).numpy()
